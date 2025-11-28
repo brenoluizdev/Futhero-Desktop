67adfb8339167685d5a32eda242b102bbb437592
Breno Luiz <brenoluizpv3@gmail.com>
2025-11-28 09:55:14 -0300

    new test with game scripts

--- src/scripts/bonk/GlobalChat.ts
+++
@@ -1,9 +0,0 @@
-(function () {
-  "use strict";
-
-  const doc = document.getElementById("descriptioncontainer");
-  
-  if (!doc) return;
-  
-  doc.innerHTML = `<iframe src="https://web.libera.chat/?nick=Guest?#bonkio" style="border:0; width:850px; height:1046px;"></iframe>`;
-})();
